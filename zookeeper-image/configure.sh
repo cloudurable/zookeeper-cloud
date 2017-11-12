0fc4b9e9ae234c961a0e4dc06a0564f7204d2c4a
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-11-11 16:02:39 -0800

    Works by reverting

--- zookeeper-image/configure.sh
+++ zookeeper-image/configure.sh
@@ -13,7 +13,7 @@ mkdir -p opt/zookeeper/data/snapshots
 cp zookeeper-src/dist-maven/zookeeper-3.4.11.jar opt/zookeeper/lib
 # Copy dependencies for zookeeper
 cp zookeeper-src/lib/*.jar opt/zookeeper/lib
-# Remove slf4j log4j jar files - can't remove all the way ZooKeeper depends on log4j
+# Remove slf4j log4j jar files - can't they have a dependency
 find opt/zookeeper/lib/ -name "*log4j*" | grep slf4j | xargs rm
 
 
@@ -22,33 +22,7 @@ find opt/zookeeper/lib/ -name "*log4j*" | grep slf4j | xargs rm
 # Copy the bin files for zookeeper
 cp zookeeper-src/bin/*.sh opt/zookeeper/bin
 
-# Create the zookeeper config file
-tee opt/zookeeper/conf/new_zoo.cfg << END
-
-dataDir=opt/zookeeper/data/snapshots
-dataLogDir=opt/zookeeper/data/transactions
-clientPort=2181
-initLimit=5
-syncLimit=2
-autopurge.purgeInterval=12
-autopurge.snapRetainCount=5
-tickTime=5000
-maxClientCnxns=0
-#clientPortAddress=
-cnxTimeout=20000
-minSessionTimeout=24000
-maxSessionTimeout=60000
-standaloneEnabled=true
-
-#ENSEMBLE0
-#ENSEMBLE1
-#ENSEMBLE2
-#ENSEMBLE3
-#ENSEMBLE4
-#ENSEMBLE5
-
-END
-
+# Create the zookeeper config file - TODO improve this
 tee opt/zookeeper/conf/zoo.cfg << END
 
 dataDir=opt/zookeeper/data/snapshots
@@ -72,38 +46,24 @@ tee opt/zookeeper/bin/run.sh << END
 #!/usr/bin/env bash
 set -e
 
-export CFG_FILE="\$ZOOCFGDIR/\$ZOOCFG"
-echo "Modifying config file \$CFG_FILE"
-sed  -i  's/opt\/zookeeper/\/opt\/zookeeper/g' \$CFG_FILE
-sed  -i  "s/clientPort=2181/clientPort=\$ZOO_CLIENT_PORT/g" \$CFG_FILE
-sed  -i  "s/initLimit=5/initLimit=\$ZOO_INIT_LIMIT/g" \$CFG_FILE
-sed  -i  "s/syncLimit=2/syncLimit=\$ZOO_SYNC_LIMIT/g" \$CFG_FILE
-sed  -i  "s/tickTime=5000/tickTime=\$ZOO_TICK_TIME/g" \$CFG_FILE
-sed  -i  "s/cnxTimeout=2000/cnxTimeout=\$ZOO_MAX_CLIENT_CNXNS/g" \$CFG_FILE
-sed  -i  "s/standaloneEnabled=true/standaloneEnabled=\$ZOO_STANDALONE_ENABLED/g" \$CFG_FILE
-sed  -i  "s/minSessionTimeout=24000/minSessionTimeout=\$ZOO_MIN_SESSION_TIMEOUT/g" \$CFG_FILE
-sed  -i  "s/maxSessionTimeout=60000/maxSessionTimeout=\$ZOO_MAX_SESSION_TIMEOUT/g" \$CFG_FILE
-echo "Modifying config file \$CFG_FILE for auto purge"
-sed  -i  "s/autopurge.purgeInterval=12/autopurge.purgeInterval=\$ZOO_AUTO_PURGE_INTERVAL/g" \$CFG_FILE
-echo "Modifying config file \$CFG_FILE for auto purge retain count"
-sed  -i  "s/autopurge.snapRetainCount=5/autopurge.snapRetainCount=\$ZOO_AUTO_PURGE_SNAP_RETAIN_COUNT/g" \$CFG_FILE
-DATA_DIR=\$(echo \$ZOO_DATA_DIR | sed 's/\//\\\\\\//g')
-echo "Modifying config file \$CFG_FILE for data dir \$DATA_DIR"
-sed  -i  "s/dataDir=\/opt\/zookeeper\/data\/snapshots/dataDir=\$DATA_DIR/g" \$CFG_FILE
-DATA_LOG_DIR=\$(echo \$ZOO_DATA_LOG_DIR | sed 's/\//\\\\\\//g')
-echo "Modifying config file \$CFG_FILE for data log dir \$DATA_LOG_DIR"
-sed  -i  "s/dataLogDir=\/opt\/zookeeper\/data\/transactions/dataLogDir=\$DATA_LOG_DIR/g" \$CFG_FILE
-
-
-
-
-if [ "x$ZOO_CLIENT_PORT_ADDRESS" != "x" ]
-then
-  echo "Modifying config file \$CFG_FILE for client port address $ZOO_CLIENT_PORT_ADDRESS"
-  sed  -i  "s/#clientPortAddress=/clientPortAddress=\$ZOO_CLIENT_PORT_ADDRESS/g" \$CFG_FILE
+if [ -z "\$CLIENT_PORT" ]; then
+    export CLIENT_PORT=2181
 fi
 
+if [ -z "\$PEER_PORT" ]; then
+    export PEER_PORT=2888
+fi
 
+if [ -z "\$LEADER_PORT" ]; then
+    export LEADER_PORT=3888
+fi
+
+if [ -z "\$MY_ID" ]; then
+    export MY_ID=1
+fi
+
+sed  -i  's/opt\/zookeeper/\/opt\/zookeeper/g' /opt/zookeeper/conf/zoo.cfg
+sed  -i  "s/clientPort=2181/clientPort=\$CLIENT_PORT/g" /opt/zookeeper/conf/zoo.cfg
 
 idx=1
 if env | grep -q ^ENSEMBLE=
@@ -111,8 +71,8 @@ then
   echo "ENSEMBLE SET \$ENSEMBLE"
   for server in \${ENSEMBLE//,/ }
   do
-    echo "#ENSEMBLE\${idx} changed to \${server}:\${ZOO_PEER_PORT}:\${ZOO_LEADER_PORT}"
-    sed -i "s/#ENSEMBLE\${idx}/server.\${idx}=\${server}:\${ZOO_PEER_PORT}:\${ZOO_LEADER_PORT}/" \$CFG_FILE
+    echo "#ENSEMBLE\${idx} changed to \${server}:\${PEER_PORT}:\${LEADER_PORT}"
+    sed -i "s/#ENSEMBLE\${idx}/server.\${idx}=\${server}:\${PEER_PORT}:\${LEADER_PORT}/" /opt/zookeeper/conf/zoo.cfg
    let "idx=idx+1"
   done
 else
@@ -120,14 +80,14 @@ else
 fi
 
 tee /opt/zookeeper/data/snapshots/myid << EOF
-\$ZOO_MY_ID
+\$MY_ID
 #myid file
 
 EOF
 
-cat \$CFG_FILE
+cat /opt/zookeeper/conf/zoo.cfg
 
-\$ZOOBINDIR/zkServer.sh start-foreground
+/opt/zookeeper/bin/zkServer.sh start-foreground
 
 
 END
